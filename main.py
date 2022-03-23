0684ecae9e1f13581d6ffd78406880899cba1f5e
Michal <mihal30060@gmail.com>
2022-03-23 11:30:50 +0100

    add decision betwenn way to chose new value

--- main.py
+++ main.py
@@ -17,46 +17,50 @@ def Fx(X):
 
 
 def find_new_key(results_tab, new_fx):
-    cout = 0
+    amount = 0
     for i in range(len(results_tab)):
         if results_tab[i].fx > new_fx:
             break
-        cout = cout + 1
-    return cout
+        amount = amount + 1
+    return amount
 
 
 def shift_key(results_tab, key):
     for i in range(len(results_tab), key, -1):
-        temp_obj = results_tab[i-1]
+        temp_obj = results_tab[i - 1]
         results_tab[i] = temp_obj
 
-    return  results_tab
+    return results_tab
 
 
 def generate_start_value(size, amount_of_var, X_max, X_min):
-    results = {}
-    results.clear()
+    results_tab = {}
+    results_tab.clear()
     obj = {}
     key = 0
 
     for i in range(size):
-        X = []
-        fx = 0
+        X = create_list_of_random_value(X_max, X_min, amount_of_var)
+        results_tab = add_new_value_to_list(X, results_tab)
+    return results_tab
 
-        for j in range(amount_of_var):
-            X.append((X_max[j] - X_min[j]) * random.random() + X_min[j])
 
-        fx = Fx(X)
-        obj = Result(X, fx)
-
-        key = find_new_key(results, fx)
-
-        if key < len(results):
-           results =  shift_key(results, key)
-
-        results[key] = obj
-
+def add_new_value_to_list(X, results):
+    fx = Fx(X)
+    obj = Result(X, fx)
+    key = find_new_key(results, fx)
+    if key < len(results):
+        results = shift_key(results, key)
+    results[key] = obj
     return results
+
+
+def create_list_of_random_value(X_max, X_min, amount_of_var):
+    X = []
+    for j in range(amount_of_var):
+        X.append(random_from_range(X_min[j], X_max[j]))
+
+    return X
 
 
 def show_table(results_table, cout_value):
@@ -72,18 +76,43 @@ def show_table(results_table, cout_value):
         print(info)
 
 
+def random_from_range(A, B):
+    return (B - A) * random.random() + A
+
+
 if __name__ == '__main__':
-    HMS = 10
+    HMS = 3
     HMCR = 0.85
     PAR = 0.45
-    b = 10
+    b = 1
     amount_of_var = 2
+    amount_of_step = 10
 
     step = 0
     X = []
-    X_max = [10, 10] # Kolejno minima i maximna dla X1,X2,...,XN
+    X_max = [10, 10]  # Kolejno minima i maximna dla X1,X2,...,XN
     X_min = [-10, -10]
 
     results_table = generate_start_value(HMS, amount_of_var, X_max, X_min)
 
+    threshold1 = HMCR * (1 - PAR)
+    threshold2 = HMCR * PAR
+    threshold3 = 1 - HMCR
+
     show_table(results_table, amount_of_var)
+    for i in range(4):
+        X_var = []
+        decision_var = random.random()
+
+        if decision_var <= threshold1:
+            print("P1")
+        elif decision_var <= (threshold2 + threshold1):
+            correction = random_from_range(-b, b)
+            print("P2")
+        else:
+            print("P3")
+            X_var = create_list_of_random_value(X_max,X_min,amount_of_var)
+            results_table = add_new_value_to_list(X_var, results_table)
+            show_table(results_table, amount_of_var)
+
+

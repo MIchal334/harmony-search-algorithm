332fe0b2841145626a7abda9bd35f5a6a373f405
Michal <mihal30060@gmail.com>
2022-03-31 09:00:11 +0200

    add frame to GUI

--- GUI.py
+++ GUI.py
@@ -65,30 +65,39 @@ def add_math_world(str, indexes):
 
 
 # Init Button and filed
-fun_filed = Entry(root, width=70)
-X_min_filed = Entry(root, width=50)
-X_max_filed = Entry(root, width=50)
-amount_var_filed = Entry(root, width=8)
-HMCR_filed = Entry(root, width=8)
-HMS_filed = Entry(root, width=8)
-PAR_filed = Entry(root, width=8)
 
-start_button = Button(text="Start",
+frame_input = LabelFrame(root, text="dane wejsciowe", padx=15, pady=15)
+frame_data = LabelFrame(root, text="parametry wejśsciowe", padx=15, pady=15)
+frame_controler = LabelFrame(root, padx=5, pady=5)
+fun_filed = Entry(frame_input, width=50)
+X_min_filed = Entry(frame_input, width=50)
+X_max_filed = Entry(frame_input, width=50)
+amount_var_filed = Entry(frame_data, width=10)
+HMCR_filed = Entry(frame_data, width=10)
+HMS_filed = Entry(frame_data, width=10)
+PAR_filed = Entry(frame_data, width=10)
+iterrations_filed = Entry(frame_data, width=10)
+
+start_button = Button(frame_controler, text="Start", padx=5, pady=5,
                       command=lambda: start_program(fun_filed.get(), X_min_filed.get(), X_max_filed.get(),
                                                     amount_var_filed.get()))
 
-clear_button = Button(text="Clear", command=clear)
-fx_text = Label(root, text="F(X) = ")
-xmin_text = Label(root, text="Xmin =")
-xmax_text = Label(root, text="Xmax =")
-amount_var_text = Label(root, text="Amount of\n Variable = ")
-HMS_text = Label(root,text="HMS=")
-HMCR_text = Label(root,text="HMCR=")
-PAR_text = Label(root,text="PAR=")
+clear_button = Button(frame_controler, text="Clear", padx=5, pady=5, command=clear)
+fx_text = Label(frame_input, text="F(X) = ")
+xmin_text = Label(frame_input, text="Xmin =")
+xmax_text = Label(frame_input, text="Xmax =")
+amount_var_text = Label(frame_data, text="Amount of\n Variable = ")
+HMS_text = Label(frame_data, text="HMS=")
+HMCR_text = Label(frame_data, text="HMCR=")
+PAR_text = Label(frame_data, text="PAR=")
+iter_text = Label(frame_data, text="Iterations=")
 
+frame_input.grid(column=0, row=0)
+frame_data.grid(column=1, row=0)
+frame_controler.grid(column=0, row=1, columnspan=2)
 # Place of components
 fx_text.grid(row=0, column=0)
-fun_filed.grid(row=0, column=1, columnspan=7, padx=10, pady=10)
+fun_filed.grid(row=0, column=1, columnspan=4, padx=10, pady=10)
 
 xmin_text.grid(row=1, column=0)
 X_min_filed.grid(row=1, column=1, columnspan=4, padx=10, pady=10)
@@ -96,17 +105,18 @@ X_min_filed.grid(row=1, column=1, columnspan=4, padx=10, pady=10)
 xmax_text.grid(row=2, column=0)
 X_max_filed.grid(row=2, column=1, columnspan=4, padx=10, pady=10)
 
-amount_var_text.grid(row=3, column=0)
-amount_var_filed.grid(row=3, column=1)
-HMS_text.grid(row=3, column=2)
-HMS_filed.grid(row=3, column=3)
-HMCR_text.grid(row=3, column=4)
-HMCR_filed.grid(row=3, column=5)
-PAR_text.grid(row=3, column=6)
-PAR_filed.grid(row=3, column=7)
+amount_var_text.grid(row=0, column=0)
+amount_var_filed.grid(row=0, column=1)
+HMS_text.grid(row=1, column=0)
+HMS_filed.grid(row=1, column=1)
+HMCR_text.grid(row=2, column=0)
+HMCR_filed.grid(row=2, column=1)
+PAR_text.grid(row=3, column=0)
+PAR_filed.grid(row=3, column=1)
+iter_text.grid(row=4, column=0)
+iterrations_filed.grid(row=4, column=1)
 
-
-start_button.grid(row=4, column=1)
-clear_button.grid(row=4, column=2)
+start_button.grid(row=0, column=0)
+clear_button.grid(row=0, column=1)
 
 root.mainloop()
